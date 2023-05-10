9ef455b1b304714184892128872d5f1747abcb2f
NicholasLysons <boylysons@icloud.com>
2023-05-09 17:19:37 -0700

    created file structure

---
+++ Main/server.js
@@ -0,0 +1,125 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // TODO: Add MySQL password here
+    password: 'root',
+    database: 'employees_db'
+  },
+  console.log(`Connected to the employees_db database.`)
+);
+
+// Create a movie
+app.post('/api/new-department', ({ body }, res) => {
+  const sql = `INSERT INTO department (department_name)
+    VALUES (?)`;
+  const params = [body.department_name];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: body
+    });
+  });
+});
+
+// Read all movies
+app.get('/api/department', (req, res) => {
+  const sql = `SELECT id, department_name AS title FROM department`;
+  
+  db.query(sql, (err, data) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+       return;
+    }
+    res.json({
+      message: 'success',
+      data
+    });
+  });
+});
+
+// Delete a movie
+app.delete('/api/department/:id', (req, res) => {
+  const sql = `DELETE FROM department WHERE id = ?`;
+  const params = [req.params.id];
+  
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.statusMessage(400).json({ error: res.message });
+    } else if (!result.affectedRows) {
+      res.json({
+      message: 'department not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+// Read list of all employees and associated departments name using LEFT JOIN
+app.get('/api/department-employee', (req, res) => {
+  const sql = `SELECT departments.department_name AS department, employees.employee FROM employees LEFT JOIN departments ON employee.department_id = departments.id ORDER BY departments.department_name;`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+// BONUS: Update employee name
+app.put('/api/employee/:id', (req, res) => {
+  const sql = `UPDATE employee SET employee = ? WHERE id = ?`;
+  const params = [req.body.employee, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'department not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+
+app.use((req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
